176c3efe522924c8e98779a19c4ebf959875e4df
Hardart <kaino@bk.ru>
2021-11-19 18:35:06 +0300

    Добавил кнопку "Выйти" и TableView на экран

--- RushHour/ViewControllers/TabBar/AppConfigVC.swift
+++ RushHour/ViewControllers/TabBar/AppConfigVC.swift
@@ -6,19 +6,67 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class AppConfigVC: UIViewController {
+    
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .cyan
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableViewLayout()
         configurationNavigationButtons()
+        
+    }
+    
+
+    func tableViewLayout(){
+        view.addSubview(tableView)
+        tableView.anchors(
+            top: view.topAnchor,
+            left: view.leftAnchor,
+            bottom: view.bottomAnchor,
+            right: view.rightAnchor
+        )
     }
     
     func configurationNavigationButtons() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(
-            title: "Выйти", style: .done, target: nil, action: nil)
-    
+            title: "Выйти", style: .done, target: self, action: #selector(tapLogOut))
     }
+    
+    @objc func tapLogOut(){
+        do {
+            try FirebaseAuth.Auth.auth().signOut()
+            let vc = LoginVC()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: true)
+        } catch {
+            print("Something wrong")
+        }
+    }
+}
+
+extension AppConfigVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Таблица"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
